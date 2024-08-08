e91228279850271d0758d8aee072646d70279747
Bryan Hoang <bryanhoang@mines.edu>
2024-08-08 16:11:49 -0600

    Initial Commit

---
+++ friends/friendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  friendsApp.swift
+//  friends
+//
+//  Created by Bryan Hoang on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct friendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
