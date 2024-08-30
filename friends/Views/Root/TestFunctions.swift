0fe0c156315c8abaa81eda16e35a0503ab128657
Bryan Hoang <bryan303.co@gmail.com>
2024-08-30 16:34:47 -0600

    Create TestFunctions.swift

---
+++ friends/Views/Root/TestFunctions.swift
@@ -0,0 +1,30 @@
+//
+//  TestFunctions.swift
+//  friends
+//
+//  Created by Bryan Hoang on 8/30/24.
+//
+
+import SwiftUI
+
+struct TestFunctions: View {
+    
+    @EnvironmentObject var avm: AuthenticationVM
+    
+    var body: some View {
+        Button {
+            do {
+                print(avm.user ?? "Error")
+            }
+        } label: {
+            Text("Print Authenticated User Data")
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        TestFunctions()
+            .environmentObject(AuthenticationVM())
+    }
+}
