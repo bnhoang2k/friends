5c3516a69b05320a1a3cc314fe9e6c4175f5b004
Bryan Hoang <bryan303.co@gmail.com>
2024-09-05 13:21:53 -0600

    Moved some code from SettingsView out to make it cleaner.

--- friends/Views/User Settings/SettingsView.swift
+++ friends/Views/User Settings/SettingsView.swift
@@ -11,7 +11,7 @@ import SwiftyCrop
 
 struct SettingsView: View {
     
-    @EnvironmentObject private var avm: AuthenticationVM
+    @EnvironmentObject var avm: AuthenticationVM
     @Environment(\.dismiss) private var dismiss
     
     @State private var showImageOptions: Bool = false
@@ -83,9 +83,7 @@ struct SettingsView: View {
                 }
             }
         }
-        .onAppear {
-            dummyUser = avm.user
-        }
+        .onAppear {dummyUser = avm.user}
         .sheet(isPresented: $showImageOptions,
                onDismiss: {
             if tempUIImage != nil { // Only show the cropper if an image was selected
@@ -133,121 +131,8 @@ struct SettingsView: View {
     }
 }
 
-// MARK: Non - email fields
-struct EditFieldView<T>: View {
-    @Binding var value: T?
-    var fieldName: String
-    var imageName: String
-    
-    var body: some View {
-        HStack {
-            Label(fieldName, systemImage: imageName)
-            Spacer()
-            if T.self == String.self {
-                TextField("Enter \(fieldName)", text: Binding(
-                    get: { value as? String ?? "" },
-                    set: { newValue in
-                        value = newValue as? T
-                    })
-                )
-                .frame(maxWidth: .infinity)
-                .multilineTextAlignment(.trailing)
-            }
-            // extend this to support other types like Int, Double, etc.
-        }
-        .padding(.horizontal)
-    }
-}
-
-struct EditPasswordView: View {
-    
-    @EnvironmentObject private var avm: AuthenticationVM
-    
-    @State private var email: String = ""
-    @State private var pwd: String = ""
-    @State private var nPwd: String = ""
-    @State private var nPwd2: String = ""
-    
-    @Environment(\.dismiss) private var dismiss
-    @State private var showAlert: Bool = false
-    
-    // TODO: If more fields are added, adjust here.
-    private var isValid: Bool {
-        Utilities.shared.is_valid_email(email: email) &&
-        email == avm.user?.email! &&
-        Utilities.shared.is_valid_password(password: nPwd) &&
-        nPwd == nPwd2
-    }
-    
-    var body: some View {
-        VStack {
-            CustomTF(filler_text: "Enter your email", text_binding: $email)
-            CustomPF(filler_text: "Enter your old password", eye: true,text_binding: $pwd)
-            CustomPF(filler_text: "Enter your new password", eye: true,text_binding: $nPwd)
-            CustomPF(filler_text: "Enter your new password again", eye: true,text_binding: $nPwd2)
-            okButton
-            Spacer()
-        }
-        .padding()
-        .navigationTitle("Edit Password")
-        .navigationBarTitleDisplayMode(.inline)
-    }
-    
-    private var okButton: some View {
-        Button {
-            Task {
-                do {
-                    try await avm.updatePassword(email: email, pwd: pwd, pwdN: nPwd)
-                } catch {
-                    print("SignUpView: Error signing up. \(error)")
-                }
-                showAlert.toggle()
-            }
-        } label: {
-            Text("OK")
-                .frame(maxWidth: .infinity)
-                .padding(5)
-                .background(RoundedRectangle(cornerRadius: GlobalVariables.shared.TEXTFIELD_RRRADIUS).fill(isValid ? Color.blue : Color.gray.opacity(0.2)))
-                .foregroundColor(isValid ? Color.white : Color(UIColor.systemGray))
-                .font(.custom(GlobalVariables.shared.APP_FONT, size: 20))
-        }
-        .disabled(!isValid)
-        .alert(isPresented: $showAlert) {
-            Alert(title: Text("A verification link has been sent to your email."), dismissButton: .default(Text("OK")){dismiss()})
-        }
-    }
-}
-
-private struct ImageOptionsView: View {
-    
-    @Environment(\.dismiss) private var dismiss
-    @Binding var selectedPhoto: PhotosPickerItem?
-    @Binding var tempUIImage: UIImage?  // Temporary UIImage for cropping
-    @Binding var showImageCropper: Bool
-    
-    var body: some View {
-        VStack {
-            HStack {
-                Label("Take a photo", systemImage: "camera")
-                Spacer()
-            }
-            PhotosPicker(selection: $selectedPhoto, matching: .images) {
-                Label("Pick from library", systemImage: "photo")
-                Spacer()
-            }
-            .onChange(of: selectedPhoto) { newPhoto in
-                dismiss()  // Dismiss after picking a photo
-            }
-            .padding(.top)
-        }
-        .padding(.horizontal)
-        .presentationDetents([.fraction(0.15)])
-    }
-}
-
 // MARK: Misc.
 extension SettingsView {
-    
     struct EditPhotoView: View {
         
         @EnvironmentObject var avm: AuthenticationVM
@@ -314,32 +199,6 @@ extension SettingsView {
             print("Error updating user information in Firestore: \(error)")
             throw error
         }
-    }
-    
-    private var deleteAccountButton: some View {
-        Button(role: .destructive) {
-            Task {
-                do {
-                    try await avm.deleteUser()
-                    avm.showSignInView = true
-                } catch {
-                    print("Error deleting user: \(error)")
-                }
-            }
-        } label: {
-            HStack {
-                Spacer()
-                Text("Delete Account")
-                    .frame(maxWidth: .infinity)
-                    .padding(.vertical)
-                    .background(
-                        RoundedRectangle(cornerRadius: GlobalVariables.shared.TEXTFIELD_RRRADIUS)
-                            .fill(Color(UIColor.secondarySystemBackground))
-                    )
-                Spacer()
-            }
-        }
-        .buttonStyle(.borderless)
     }
 }
 
---
+++ friends/Views/User Settings/SettingsViewExtras.swift
@@ -0,0 +1,149 @@
+//
+//  SettingsViewExtras.swift
+//  friends
+//
+//  Created by Bryan Hoang on 9/5/24.
+//
+
+import SwiftUI
+import PhotosUI
+
+extension SettingsView {
+    struct ImageOptionsView: View {
+        
+        @Environment(\.dismiss) private var dismiss
+        @Binding var selectedPhoto: PhotosPickerItem?
+        @Binding var tempUIImage: UIImage?  // Temporary UIImage for cropping
+        @Binding var showImageCropper: Bool
+        
+        var body: some View {
+            VStack {
+                HStack {
+                    Label("Take a photo", systemImage: "camera")
+                    Spacer()
+                }
+                PhotosPicker(selection: $selectedPhoto, matching: .images) {
+                    Label("Pick from library", systemImage: "photo")
+                    Spacer()
+                }
+                .onChange(of: selectedPhoto) { newPhoto in
+                    dismiss()  // Dismiss after picking a photo
+                }
+                .padding(.top)
+            }
+            .padding(.horizontal)
+            .presentationDetents([.fraction(0.15)])
+        }
+    }
+    
+    // MARK: Non - email fields
+    struct EditFieldView<T>: View {
+        @Binding var value: T?
+        var fieldName: String
+        var imageName: String
+        
+        var body: some View {
+            HStack {
+                Label(fieldName, systemImage: imageName)
+                Spacer()
+                if T.self == String.self {
+                    TextField("Enter \(fieldName)", text: Binding(
+                        get: { value as? String ?? "" },
+                        set: { newValue in
+                            value = newValue as? T
+                        })
+                    )
+                    .frame(maxWidth: .infinity)
+                    .multilineTextAlignment(.trailing)
+                }
+                // extend this to support other types like Int, Double, etc.
+            }
+            .padding(.horizontal)
+        }
+    }
+    
+    struct EditPasswordView: View {
+        
+        @EnvironmentObject private var avm: AuthenticationVM
+        
+        @State private var email: String = ""
+        @State private var pwd: String = ""
+        @State private var nPwd: String = ""
+        @State private var nPwd2: String = ""
+        
+        @Environment(\.dismiss) private var dismiss
+        @State private var showAlert: Bool = false
+        
+        // TODO: If more fields are added, adjust here.
+        private var isValid: Bool {
+            Utilities.shared.is_valid_email(email: email) &&
+            email == avm.user?.email! &&
+            Utilities.shared.is_valid_password(password: nPwd) &&
+            nPwd == nPwd2
+        }
+        
+        var body: some View {
+            VStack {
+                CustomTF(filler_text: "Enter your email", text_binding: $email)
+                CustomPF(filler_text: "Enter your old password", eye: true,text_binding: $pwd)
+                CustomPF(filler_text: "Enter your new password", eye: true,text_binding: $nPwd)
+                CustomPF(filler_text: "Enter your new password again", eye: true,text_binding: $nPwd2)
+                okButton
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("Edit Password")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        
+        private var okButton: some View {
+            Button {
+                Task {
+                    do {
+                        try await avm.updatePassword(email: email, pwd: pwd, pwdN: nPwd)
+                    } catch {
+                        print("SignUpView: Error signing up. \(error)")
+                    }
+                    showAlert.toggle()
+                }
+            } label: {
+                Text("OK")
+                    .frame(maxWidth: .infinity)
+                    .padding(5)
+                    .background(RoundedRectangle(cornerRadius: GlobalVariables.shared.TEXTFIELD_RRRADIUS).fill(isValid ? Color.blue : Color.gray.opacity(0.2)))
+                    .foregroundColor(isValid ? Color.white : Color(UIColor.systemGray))
+                    .font(.custom(GlobalVariables.shared.APP_FONT, size: 20))
+            }
+            .disabled(!isValid)
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("A verification link has been sent to your email."), dismissButton: .default(Text("OK")){dismiss()})
+            }
+        }
+    }
+    
+    var deleteAccountButton: some View {
+        Button(role: .destructive) {
+            Task {
+                do {
+                    try await avm.deleteUser()
+                    avm.showSignInView = true
+                } catch {
+                    print("Error deleting user: \(error)")
+                }
+            }
+        } label: {
+            HStack {
+                Spacer()
+                Text("Delete Account")
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical)
+                    .background(
+                        RoundedRectangle(cornerRadius: GlobalVariables.shared.TEXTFIELD_RRRADIUS)
+                            .fill(Color(UIColor.secondarySystemBackground))
+                    )
+                Spacer()
+            }
+        }
+        .buttonStyle(.borderless)
+    }
+}
