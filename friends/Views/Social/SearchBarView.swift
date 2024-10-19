4a6cc949488a9d1eaa4e0bbccfc6d67ebfe7309f
Bryan Hoang <bryan303.co@gmail.com>
2024-10-19 15:11:17 -0600

    Made it prettier to look like AddHangoutsView

--- friends/Views/Social/SearchBarView.swift
+++ friends/Views/Social/SearchBarView.swift
@@ -16,73 +16,143 @@ struct SearchBarView: View {
     @Environment(\.dismiss) private var dismiss
     
     @State private var toUserId: String? = nil
-    
+    @State private var keyboardHeight: CGFloat = 0.0
+
     var body: some View {
         VStack {
+            Text("Search and Add Friends")
+                .font(.headline)
+                .fontWeight(.bold)
+                .padding(.vertical)
+
+            // Search bar at the top
             HStack {
-                TextField("Search", text: $tvm.searchText)
+                TextField("Search for friends", text: $tvm.searchText)
                     .padding(10)
-                    .background(Color(.systemGray6))
+                    .background(Color(.secondarySystemBackground))
                     .cornerRadius(8)
                     .onChange(of: tvm.searchText) { _ in
                         Task {
-                            await tvm.searchUsers(query: tvm.searchText, excludedName: avm.user?.username ?? "Error")
+                            await tvm.searchUsers(
+                                query: tvm.searchText,
+                                excludedName: avm.user?.username ?? "Error"
+                            )
                         }
                     }
-                Button {
-                    Task {
-                        guard let fromUserId = avm.user?.uid,
-                              let fromUsername = avm.user?.username,
-                              let fromUserPP = avm.user?.photoURL,
-                              let toUserId = toUserId else {
-                            print("Error: Missing user details")
-                            return
+            }
+            .padding(.horizontal)
+
+            // ScrollView for search results
+            if !tvm.searchText.isEmpty {
+                ScrollView {
+                    VStack(spacing: 10) {
+                        ForEach(tvm.searchResults, id: \.uid) { user in
+                            HStack {
+                                ImageView(urlString: user.photoURL, pictureWidth: 50)
+                                VStack(alignment: .leading) {
+                                    Text(user.username ?? "username error")
+                                    Text(user.fullName ?? "full name error")
+                                        .font(.subheadline)
+                                        .foregroundColor(.gray)
+                                }
+                                Spacer()
+                                Button(action: {
+                                    withAnimation {
+                                        selectUser(user)
+                                    }
+                                }) {
+                                    Image(systemName: "plus.circle")
+                                        .foregroundColor(.blue)
+                                }
+                            }
+                            .padding(.horizontal)
                         }
-                        try await svm.sendFriendRequest(fromUserId: fromUserId,
-                                                        fromUsername: fromUsername,
-                                                        fromUserPP: [fromUserPP],
-                                                        toUserId: toUserId)
-                        dismiss()
                     }
-                } label: {
-                    Text("Add Friend")
-                        .bold()
+                    .padding([.horizontal, .top])
                 }
             }
-            .padding()
-            List(tvm.searchResults, id: \.uid) { user in
-                HStack {
-                    ImageView(urlString: user.photoURL, pictureWidth: 50)
-                    VStack(alignment: .leading) {
-                        Text(user.username ?? "username error")
-                        Text(user.fullName ?? "full name error")
-                            .font(.subheadline)
-                            .foregroundColor(.gray)
-                    }
-                    Spacer()
-                }
-                .contentShape(.rect)
-                .onTapGesture {
-                    guard let username = user.username else {return}
-                    tvm.searchText = username
-                    toUserId = user.uid
-                }
-            }
-            .listStyle(PlainListStyle())
+
             Spacer()
+
+            // "Add Friend" button at the bottom
+            Button(action: addFriend) {
+                Text("Add Friend")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(8)
+            }
+            .padding([.horizontal, .bottom])
         }
-        .navigationTitle("Search and Add Friends")
+        .offset(y: -self.keyboardHeight)
+        .animation(.spring(), value: keyboardHeight)
+        .onAppear {
+            NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification,
+                                                   object: nil,
+                                                   queue: .main) { (notification) in
+                guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+                self.keyboardHeight = keyboardFrame.height
+            }
+            NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification,
+                                                   object: nil,
+                                                   queue: .main) { (notification) in
+                self.keyboardHeight = 0
+            }
+        }
         .navigationBarTitleDisplayMode(.inline)
         .onDisappear {
             UIApplication.shared.dismissKeyboard()
-            Task {
-                tvm.searchText = ""
-                tvm.searchResults = []
-                toUserId = ""
+            clearSearchState()
+        }
+    }
+
+    // Select user action
+    private func selectUser(_ user: DBUser) {
+        tvm.searchText = user.username ?? ""
+        toUserId = user.uid
+    }
+
+    // Add friend action
+    private func addFriend() {
+        Task {
+            guard let fromUserId = avm.user?.uid,
+                  let fromUsername = avm.user?.username,
+                  let fromUserPP = avm.user?.photoURL,
+                  let toUserId = toUserId else {
+                print("Error: Missing user details")
+                return
             }
+            try await svm.sendFriendRequest(
+                fromUserId: fromUserId,
+                fromUsername: fromUsername,
+                fromUserPP: [fromUserPP],
+                toUserId: toUserId
+            )
+            dismiss()
+        }
+    }
+
+    // Clear search state when view disappears
+    private func clearSearchState() {
+        Task {
+            tvm.searchText = ""
+            tvm.searchResults = []
+            toUserId = ""
         }
     }
 }
+
+#Preview {
+    NavigationStack {
+        SearchBarView()
+            .environmentObject(TypesenseVM())
+            .environmentObject(AuthenticationVM())
+            .environmentObject(SocialVM())
+    }
+}
+
 
 extension UIApplication {
     func dismissKeyboard() {
