d64a739d3308a0d9c482fc30ad4a72766d8dd25e
Bryan Hoang <bryan303.co@gmail.com>
2024-12-11 17:17:59 -0700

    Create FriendUtilities.swift

---
+++ friends/Classes/FriendUtilities.swift
@@ -0,0 +1,36 @@
+//
+//  FriendUtilities.swift
+//  friends
+//
+//  Created by Bryan Hoang on 12/11/24.
+//
+
+class FriendUtilities {
+    func calculateFriendStatistics(for friendId: String,
+                                   from hangouts: [Hangout]) -> (personalityGradient: [String: Any],
+                                                                 hardStats: [String: Any]) {
+        // Personality Gradient
+        let vibeCounts = Dictionary(grouping: hangouts, by: { $0.vibe }).mapValues { $0.count }
+        let mostCommonVibe = vibeCounts.max(by: { $0.value < $1.value })?.key
+        let indoorOutdoorRatio = Double(hangouts.filter { $0.isOutdoor }.count) / Double(hangouts.count)
+        let durationCounts = Dictionary(grouping: hangouts, by: { $0.duration }).mapValues { $0.count }
+        let mostCommonDuration = durationCounts.max(by: { $0.value < $1.value })?.key
+        
+        // Hard Stats
+        let totalHangouts = hangouts.count
+        let averageBudget = hangouts.map { $0.budget }.reduce(0, +) / Double(hangouts.count)
+        
+        return (
+            personalityGradient: [
+                "mostCommonVibe": mostCommonVibe?.rawValue ?? "Unknown",
+                "indoorOutdoorRatio": indoorOutdoorRatio,
+                "mostCommonDuration": mostCommonDuration?.rawValue ?? "Unknown"
+            ],
+            hardStats: [
+                "totalHangouts": totalHangouts,
+                "averageBudget": averageBudget,
+                "vibesCounts" : vibeCounts,
+            ]
+        )
+    }
+}
