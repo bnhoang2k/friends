38e217d3820fc0e1255273cff5aac72a7583b1ec
Bryan Hoang <bryan303.co@gmail.com>
2024-12-13 21:00:55 -0700

    Weird edge case issue

--- friends/Classes/FriendUtilities.swift
+++ friends/Classes/FriendUtilities.swift
@@ -12,13 +12,13 @@ class FriendUtilities {
         // Personality Gradient
         let vibeCounts = Dictionary(grouping: hangouts, by: { $0.vibe }).mapValues { $0.count }
         let mostCommonVibe = vibeCounts.max(by: { $0.value < $1.value })?.key
-        let indoorOutdoorRatio = Double(hangouts.filter { $0.isOutdoor }.count) / Double(hangouts.count)
+        let indoorOutdoorRatio = hangouts.isEmpty ? 0 : Double(hangouts.filter { $0.isOutdoor }.count) / Double(hangouts.count)
         let durationCounts = Dictionary(grouping: hangouts, by: { $0.duration }).mapValues { $0.count }
         let mostCommonDuration = durationCounts.max(by: { $0.value < $1.value })?.key
         
         // Hard Stats
         let totalHangouts = hangouts.count
-        let averageBudget = hangouts.map { $0.budget }.reduce(0, +) / Double(hangouts.count)
+        let averageBudget = hangouts.isEmpty ? 0 : hangouts.map { $0.budget }.reduce(0, +) / Double(hangouts.count)
         
         return (
             personalityGradient: [
@@ -29,7 +29,7 @@ class FriendUtilities {
             hardStats: [
                 "totalHangouts": totalHangouts,
                 "averageBudget": averageBudget,
-                "vibesCounts" : vibeCounts,
+                "vibesCounts": vibeCounts,
             ]
         )
     }
