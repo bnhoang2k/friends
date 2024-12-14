435c3312b96c84d3cdd266a3a87cc19b20700e17
Bryan Hoang <bryan303.co@gmail.com>
2024-12-13 17:37:54 -0700

    Removed MarkdownUI

--- friends/Misc/Utilities.swift
+++ friends/Misc/Utilities.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 import SwiftUI
-import MarkdownUI
 
 final class Utilities {
     
--- friends/Views/Social/Hangout Views/GenerateLocationsView.swift
+++ friends/Views/Social/Hangout Views/GenerateLocationsView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import MarkdownUI
 
 struct GenerateLocationsView: View {
     
